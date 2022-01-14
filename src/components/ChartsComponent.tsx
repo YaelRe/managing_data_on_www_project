393d8ff2522b360cf0b315e940a39407caba7f2c
YaelRe <yaelreches3@gmail.com>
2022-01-14 11:22:13 +0200

    add pie chart

---
+++ src/components/ChartsComponent.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import * as am5 from "@amcharts/amcharts5";
+import * as am5percent from "@amcharts/amcharts5/percent";
+import {Root} from "@amcharts/amcharts5";
+
+import * as am5xy from "@amcharts/amcharts5/xy"; // TODO: add another chart?
+
+export interface ChartsComponentProps {
+    pieChartsData: any[];
+}
+
+export const ChartsComponent: React.FC<ChartsComponentProps> = ({
+    pieChartsData,
+}) => {
+    const chartRoot = React.useRef <Root|null>(null);
+
+        React.useEffect(() => {
+            const createPieChart = () => {
+                chartRoot.current && chartRoot.current.dispose();
+                chartRoot.current = am5.Root.new('pie-chart');
+
+                let pieChart = chartRoot.current.container.children.push(
+                    am5percent.PieChart.new(chartRoot.current, {})
+                );
+                let pieSeries = pieChart.series.push(
+                    am5percent.PieSeries.new(chartRoot.current, {
+                        name: "Series",
+                        valueField: "votes",
+                        categoryField: "answer"
+                    })
+                );
+                pieSeries.data.setAll(pieChartsData);
+
+                // let pieLegend = pieChart.children.push(am5.Legend.new(chartRoot.current, {
+                //     centerX: am5.percent(50),
+                //     x: am5.percent(50),
+                //     layout: chartRoot.current.horizontalLayout
+                // }));
+                // pieLegend.data.setAll(pieSeries.dataItems);
+
+            }; createPieChart();
+
+    }, [pieChartsData]);
+
+        //TODO: move and change style (to css)
+
+    return (
+        <div className='charts-container' style={{marginTop: '50px',  display: 'flex'}}>
+            <div id='pie-chart' style={{width: "50%", height: "250px", flex: "1"}} />
+        </div>
+    );
+};
+
+export default ChartsComponent;
--- src/components/Pages/PollResults.tsx
+++ src/components/Pages/PollResults.tsx
@@ -1,12 +1,141 @@
-import React from 'react';
 import '../../App.css';
+import React from "react";
+import Select, {SingleValue} from 'react-select'
+import * as am5 from "@amcharts/amcharts5";
+import * as am5percent from "@amcharts/amcharts5/percent";
+import am4themes_animated from "@amcharts/amcharts5/themes/animated";
+import {ChartsComponent} from "../ChartsComponent";
 
+// am5.useTheme(am4themes_animated);
 
-export const CreateFilteredPoll  = () => {
+export const PollResults = () => {
 
-    return (
-        <div className='header-container'>
-        </div>
-    )
+    const [pollsListOptions, setPollsListOptions] = React.useState<any[]>([]);
+    const [pollAnswersList, setPollAnswersList] = React.useState<any[]>([]);
+    const [isPollSelected, setIsPollSelected] = React.useState<Boolean>(false);
+    const [selectedPollId, setSelectedPollId] = React.useState<number>(-1);
+    const [pieChartsData, setPieChartsData] = React.useState<any[]>([]);
+    // const chartsRoot = React.useRef<any>(null);
 
+    React.useEffect(() => {
+        const fetchPollsData = async () => {
+            let serverResponse;
+            let parsedServerResponse;
+            try {
+                serverResponse = await fetch(`http://127.0.0.1:5000/admins/get-polls-list`);
+                parsedServerResponse = await serverResponse.json();
+
+            } catch (error) {
+                console.log(error);
+            }
+
+            if (serverResponse && serverResponse.status === 200 && parsedServerResponse["polls_list"].length !== 0){
+                const tempList = parsedServerResponse["polls_list"];
+                const tempPollsListOptions: any[] = [];
+                tempList.map((poll:any) =>{
+                    const newJsonPoll: any ={
+                        value: poll[0],
+                        label: poll[1],
+                    };
+                    tempPollsListOptions.push((newJsonPoll));
+
+                });
+                setPollsListOptions(tempPollsListOptions);
+            }
+            // TODO: handle 500 status (empty list, internal error)
+            // if (chartsRoot !== null) {
+            //     chartsRoot.current = am5.Root.new("chartDiv");
+            // }
+        };
+        fetchPollsData();
+
+    }, []);
+
+    React.useEffect(() => {
+        const fetchPollsAnswers = async () => {
+            // const fetchPollsAnswers = async (poll_id: number) => {
+            // debugger;
+            let serverResponse;
+            let parsedServerResponse;
+            try {
+                serverResponse = await fetch(`http://127.0.0.1:5000/admins/get-poll-answers/${selectedPollId}`);
+                parsedServerResponse = await serverResponse.json();
+
+            } catch (error) {
+                console.log(error);
+            }
+            if (serverResponse && serverResponse.status === 200) {
+                const tempList = parsedServerResponse["polls_answers_list"];
+                const tempPollsAnswersListOptions: any[] = [];
+                tempList.map((poll_answer: string) => {
+                    const newJsonAnswer: any = {
+                        value: poll_answer,
+                        label: poll_answer,
+                    };
+                    tempPollsAnswersListOptions.push((newJsonAnswer));
+                });
+                setPollAnswersList(tempPollsAnswersListOptions);
+                setIsPollSelected(true);
+            }
+            // debugger;
+            // TODO: handle 500 status (empty list, internal error)
+            // };
+        };
+        fetchPollsAnswers();
+
+    }, [selectedPollId, setSelectedPollId]);
+
+    React.useEffect(() => {
+        const fetchPollsUsersAnswers = async () => {
+            // debugger;
+            let serverResponse;
+            let parsedServerResponse;
+            try {
+                serverResponse = await fetch(`http://127.0.0.1:5000/admins/get-poll-user-answers/${selectedPollId}`);
+                parsedServerResponse = await serverResponse.json();
+
+            } catch (error) {
+                console.log(error);
+            }
+            if (serverResponse && serverResponse.status === 200){
+                const tempUsersResultsDict = parsedServerResponse["poll_users_answers"];
+
+                const chartData: any[] = [];
+                pollAnswersList.map((poll_answer_obj:any) =>{
+                    const poll_answer = poll_answer_obj["label"];
+                    const newJsonAnswerVotes: any ={
+                        answer: [poll_answer],
+                        votes: tempUsersResultsDict[poll_answer],
+                    };
+                     chartData.push((newJsonAnswerVotes));
+                });
+                setPieChartsData(chartData);
+                // debugger;
+                // setIsPollSelected(true);
+            }
+            // TODO: handle 500 status (empty list, internal error)
+        }; fetchPollsUsersAnswers();
+
+    }, [setIsPollSelected,setPollAnswersList, pollAnswersList]);
+
+     const handlePollSelected = (event: SingleValue<{ value: number, label: string }>) => {
+         setIsPollSelected(false);
+         if (event !== null){
+             setSelectedPollId(event.value);
+             //fetchPollsAnswers();
+             // fetchPollsUsersAnswers(event.value);
+         }
+    };
+
+        return (
+        <>
+            <h2> Choose poll to see it's results:</h2>
+            <div className='polls-list-container'>
+                <Select options={pollsListOptions} onChange={handlePollSelected} placeholder={"Select poll..."}/>
+            </div>
+            <div className='charts-component'>
+                <ChartsComponent pieChartsData={pieChartsData}/>
+            </div>
+        </>
+    );
 };
